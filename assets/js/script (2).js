935d1475faa991da231ecf229ca439416885ba02
Yagholaaa <Yagho.chinaglia@etec.sp.gov.br>
2023-10-19 21:43:59 -0300

    atualização geral

---
+++ assets/js/script (2).js
@@ -0,0 +1,6 @@
+const obj = document.querySelector('.front')
+const girar = document.querySelector ('.anime1')
+
+girar.addEventListener('click', () => {
+  obj.style.transform = 'rotate(90deg)'
+})
--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-const obj = document.querySelector('.front')
-const girar = document.querySelector ('.anime1')
-
-girar.addEventListener('click', () => {
-  obj.style.transform = 'rotate(90deg)'
-})
